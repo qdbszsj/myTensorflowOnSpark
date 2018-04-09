387d42ccfa678f8714b1c4142cbe7833ca253fca
Zhang Shijing / Parker <qdbszsj@163.com>
2018-04-09 23:12:53 +0800

    Update wiki2csv.py

--- wiki2csv.py
+++ wiki2csv.py
@@ -14,36 +14,7 @@ python wiki2csv.py --test_size 0.4 --face_width 50 --max_age 5 --min_age 1
 下面这个截图表示40%用于测试集，人脸宽高50像素，年龄范围[1,5]
 '''
 # ==============================================================================
-#coding=utf-8
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-#coding=utf-8
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
+
 
 from __future__ import absolute_import
 from __future__ import division
@@ -143,6 +114,7 @@ def main(db_path, db_name, test_size, face_width, max_age, min_age):
     F.close()
 
     test_sets['age'].to_csv('test_label.csv',header=True,index=False, sep=",")
+    m,n=test_sets.shape
     F = open("test_set","w") 
     for i in range(m):
         F.write(test_sets['file_name'].values[i])
