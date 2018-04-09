be11c47eac773b31cd855b1e42a037f6da9909f2
Zhang Shijing / Parker <qdbszsj@163.com>
2018-04-10 00:47:58 +0800

    Update wiki_spark.py

--- wiki_spark.py
+++ wiki_spark.py
@@ -15,9 +15,9 @@ import tensorflow as tf
 from datetime import datetime
 
 from tensorflowonspark import TFCluster
-import mnist_dist
+import wiki_dist
 
-sc = SparkContext(conf=SparkConf().setAppName("mnist_spark"))
+sc = SparkContext(conf=SparkConf().setAppName("wiki_spark"))
 executors = sc._conf.get("spark.executor.instances")
 num_executors = int(executors) if executors is not None else 1
 num_ps = 1
@@ -26,9 +26,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--batch_size", help="number of records per batch", type=int, default=100)
 parser.add_argument("--epochs", help="number of epochs", type=int, default=1)
 parser.add_argument("--format", help="example format: (csv|pickle|tfr)", choices=["csv", "pickle", "tfr"], default="csv")
-parser.add_argument("--images", help="HDFS path to MNIST images in parallelized format")
-parser.add_argument("--labels", help="HDFS path to MNIST labels in parallelized format")
-parser.add_argument("--model", help="HDFS path to save/load model during train/inference", default="mnist_model")
+parser.add_argument("--images", help="HDFS path to wiki images in parallelized format")
+parser.add_argument("--labels", help="HDFS path to wiki labels in parallelized format")
+parser.add_argument("--model", help="HDFS path to save/load model during train/inference", default="wiki_model")
 parser.add_argument("--cluster_size", help="number of nodes in the cluster", type=int, default=num_executors)
 parser.add_argument("--output", help="HDFS path to save test/inference output", default="predictions")
 parser.add_argument("--readers", help="number of reader/enqueue threads", type=int, default=1)
@@ -57,7 +57,7 @@ if args.format == "tfr":
   dataRDD = images.map(lambda x: toNumpy(bytes(x[0])))
 else:
   if args.format == "csv":
-    images = sc.textFile(args.images).map(lambda ln: [int(x) for x in ln.split(',')])
+    images = sc.textFile(args.images).map(lambda ln: [ord(x) for x in ln.split(',')])
     labels = sc.textFile(args.labels).map(lambda ln: [float(x) for x in ln.split(',')])
   else:  # args.format == "pickle":
     images = sc.pickleFile(args.images)
@@ -65,7 +65,7 @@ else:
   print("zipping images and labels")
   dataRDD = images.zip(labels)
 
-cluster = TFCluster.run(sc, mnist_dist.map_fun, args, args.cluster_size, num_ps, args.tensorboard, TFCluster.InputMode.SPARK, log_dir=args.model)
+cluster = TFCluster.run(sc, wiki_dist.map_fun, args, args.cluster_size, num_ps, args.tensorboard, TFCluster.InputMode.SPARK, log_dir=args.model)
 if args.mode == "train":
   cluster.train(dataRDD, args.epochs)
 else:
