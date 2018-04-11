998a8c5d74402d23793b4f74011d554cace51bf9
Zhang Shijing / Parker <qdbszsj@163.com>
2018-04-11 23:42:02 +0800

    Update wiki_data_Transfer_ForNeo.py

--- wiki_data_Transfer_ForNeo.py
+++ wiki_data_Transfer_ForNeo.py
@@ -1,27 +1,15 @@
-
-
-
-import pandas as pd
+'''
+把wiki2csv的train_set或者test_set转化成用int表示的csv，即每一行都是用","隔开的int值。
+imageWidth是之前选取的图片宽度，如果有100张图片，宽度是20，那么读入的文件是一个长度为100*20*20*3的字符串
+最终输出的csv文件是100行，每行是20*20*3共1200个用逗号隔开的0-255的int值
+'''
 import numpy as np
-'''
-train=pd.read_csv('test_set.csv', delimiter=",")
-m,n=train.shape
-print(m,n)
-'''
-
-#F = open("train_set","rb") 
-#train_set=F.read() 
-
-#train=pd.read_csv('train_label.csv', delimiter=",")
-
-#print(ord(all[1]))
-
-
-
+#这里参数设置，很重要
+###########################################################################
 imagePath="train_set"
 imageWidth=50
-#labelPath="train_label.csv"
 savePath="neo_train_set"
+###########################################################
 
 imagesFile = open(imagePath,"rb") 
 imagesString=imagesFile.read() 
@@ -37,12 +25,7 @@ for i in range(totalLen):
 images=np.array(imageList).reshape(number, singleLen)
 images.astype(int)
 
-#labelFile=pd.read_csv(labelPath, delimiter=",")
-#labels=np.array(labelFile['age'].values[:]).reshape(number,1)
-
-print(images.shape)#m * 76800
-#print(labels.shape)#m * 1
-
+print(images.shape)#m * 7500
 
 np.savetxt('neo_train_set', images, fmt="%d", delimiter=",")
 
