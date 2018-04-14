74dfe5dbb8497dcac5380a45082cb747cd8d3e8c
Zhang Shijing / Parker <qdbszsj@163.com>
2018-04-14 17:38:24 +0800

    Update wiki_data_Transfer_ForNeo.py

--- wiki_data_Transfer_ForNeo.py
+++ wiki_data_Transfer_ForNeo.py
@@ -1,8 +1,12 @@
 #encoding=utf-8
 '''
-把wiki2csv的train_set或者test_set转化成用int表示的csv，即每一行都是用","隔开的int值。
+这个要配合wiki2csv和wiki_mergeData来使用，先wiki2csv存到了myData文件夹下，如果有多个进程，存成好几份，要先运行wiki_mergeData.py合并成
+train_set,test_set,train_label,testlabel
+然后运行这个脚本，把四个文件合成一个文件。名为nei_set
+用int表示的csv，即每一行都是用","隔开的int值。
+第一列是label，一个1-100的int值
 imageWidth是之前选取的图片宽度，如果有100张图片，宽度是20，那么读入的文件是一个长度为100*20*20*3的字符串
-最终输出的csv文件是100行，每行是20*20*3共1200个用逗号隔开的0-255的int值
+最终输出的csv文件是100行，每行是20*20*3共1200个用逗号隔开的0-255的int值，第一列再加一个label
 '''
 import numpy as np
 import pandas as pd
